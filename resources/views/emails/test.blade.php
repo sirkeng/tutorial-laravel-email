bf0f84955a821a1ebb84ce2971bfbedcc0c865b1
keng <naykeng@gmail.com>
2017-07-05 09:47:04 +0700

    Completed part of email

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<h1>{{$title}}</h1>
+
+	<p>{{$content}}</p>
+
+
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+use Illuminate\Support\Facades\Mail;
+
+
+
+Route::get('/', function () {
+    // return view('welcome');
+
+
+	$data = [
+
+		'title'=> 'Hi student I hope you like the course',
+		'content'=> 'This laravel course was created with a lot of love and dedication for you'
+
+
+	];
+
+
+	Mail::send('emails.test', $data, function($message){
+
+
+		$message->to('naykeng1kz1@gmail.com', 'SirKenG')->subject('Hello student how are you?');
+
+
+	});
+
+
+
+
+});
